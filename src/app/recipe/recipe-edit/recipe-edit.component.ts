9ed7f42973dbafecf18c25e705845b2ab0f84669
rsantoshreddy <rsantoshreddy09@gmail.com>
2019-07-08 00:18:21 +0530

    Chapter-16: Form - reactive form

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { RecipeService } from '../recipe-service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.less']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode: Boolean = false;
+  recipeForm: FormGroup;
 
-  constructor() { }
-
+  constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipeService) {}
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] !== null;
+      this.initForm();
+    });
   }
-
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl()
+      })
+    );
+  }
+  private initForm() {
+    let name = '';
+    let imagePath = '';
+    let description = '';
+    let ingredients = new FormArray([]);
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      name = recipe.name;
+      imagePath = recipe.imagePath;
+      description = recipe.description;
+      if (recipe.ingredients) {
+        for (let ingredient of recipe.ingredients) {
+          ingredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(name),
+      imagePath: new FormControl(imagePath),
+      description: new FormControl(description),
+      ingredients: ingredients
+    });
+  }
 }
